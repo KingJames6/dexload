41492b23f3d92c3bc9832e0c5c51244b3b253fb2
KJames <he7212wei@gmail.com>
2018-02-27 00:05:45 +0800

    dexload加载插件apk的资源文件

--- app/src/main/java/com/hewei26/load/MainActivity.java
+++ app/src/main/java/com/hewei26/load/MainActivity.java
@@ -1,9 +1,14 @@
 package com.hewei26.load;
 
+import android.content.Context;
 import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -20,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TextView tv = findViewById(R.id.tv);
+        ImageView iv = findViewById(R.id.iv);
+
+        //复制apk到内部目录
         AssetManager assets = this.getAssets();
         InputStream is = null;
         try {
@@ -27,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
             int len = 0;
             byte buffer[] = new byte[1024];
             File file = new File(this.getExternalFilesDir("") ,"bundle.apk");
+            if(file.exists()) file.delete();
             file.createNewFile();
 
             FileOutputStream fos = new FileOutputStream(file);
@@ -38,11 +48,32 @@ public class MainActivity extends AppCompatActivity {
 
             loadApk(file.getAbsolutePath());
 
+            /**
+             *  插件资源对象
+             */
+            Resources resources = getBundleResource(this,file.getAbsolutePath());
+            /**
+             *获取图片资源
+             */
+            Drawable drawable = resources.getDrawable(resources.getIdentifier("test", "drawable",
+                    "com.hewei26.beloaded"));
+            /**
+             *  获取文本资源
+             */
+            String text = resources.getString(resources.getIdentifier("text_beload","string",
+                    "com.hewei26.beloaded"));
+
+            iv.setImageDrawable(drawable);
+            tv.setText(text);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+    /**
+     * 加载插件apk
+     * @param apkPath
+     */
     public void loadApk(String apkPath) {
         Log.v("loadDexClasses", "Dex Preparing to loadDexClasses!");
         File file = this.getExternalFilesDir("dexOpt");
@@ -69,5 +100,22 @@ public class MainActivity extends AppCompatActivity {
         } catch (InvocationTargetException e) {
             e.printStackTrace();
         }
+    }
+
+    public Resources getBundleResource(Context context, String apkPath){
+        AssetManager assetManager = createAssetManager(apkPath);
+        return new Resources(assetManager, context.getResources().getDisplayMetrics(), context.getResources().getConfiguration());
+    }
+
+    private AssetManager createAssetManager(String apkPath) {
+        try {
+            AssetManager assetManager = AssetManager.class.newInstance();
+            AssetManager.class.getDeclaredMethod("addAssetPath", String.class).invoke(
+                    assetManager, apkPath);
+            return assetManager;
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+        return null;
     }
 }
